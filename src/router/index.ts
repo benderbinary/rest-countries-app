b09c10346c91742d8be2c92258bb9d50d587aa5c
Eldar Hasanbasic <e.hasanbasic@cheqsite.com>
2024-07-19 17:37:21 +0200

    CountryView component in router

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,10 +1,12 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import Home from '../views/Home.vue';
+import CountryView from '../views/CountryView.vue';
 
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
     routes: [
         { path: '/', name: 'home', component: Home },
+        { path: '/country/:code', name: 'CountryView', component: CountryView },
     ],
 });
 
