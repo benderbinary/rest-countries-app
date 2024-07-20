7c0cd8e1048d6b4de3842bb1d2a0c33d7c71fe2a
Eldar Hasanbasic <e.hasanbasic@cheqsite.com>
2024-07-20 15:23:33 +0200

    Build fix WIP

--- src/components/CountryCard.vue
+++ src/components/CountryCard.vue
@@ -17,12 +17,18 @@
 <script setup lang="ts">
 import { PropType } from "vue";
 
-const props = defineProps({
+const { country } = defineProps<{
     country: {
-        type: Object as PropType<any>,
-        required: true,
-    },
-});
+        flags: {
+            png: string;
+        },
+        name: string;
+        population: number;
+        region: string;
+        capital: string;
+        alpha3Code: string;
+    };
+}>();
 </script>
 
 <style scoped>
--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -2,11 +2,22 @@
     <nav class="navbar">
         <div class="container">
             <h1 class="logo">Where in the world?</h1>
+            <button @click="toggleDarkMode">Dark Mode</button>
         </div>
     </nav>
 </template>
 
 <script setup lang="ts">
+import { ref } from 'vue';
+
+const isDarkMode = ref(false);
+const toggleDarkMode = () => {
+    isDarkMode.value = !isDarkMode.value;
+    document.documentElement.setAttribute(
+        "data-theme",
+        isDarkMode.value ? "dark" : "light"
+    );
+};
 </script>
 
 <style scoped lang="scss">
@@ -19,11 +30,24 @@
     box-shadow: 0 2px 4px rgba(0, 0, 0, 0.05);
     padding: 1rem 3rem;
 
-    .container {}
+    .container {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        width: 100%;
+        /* As per style guide */
+        max-width: 1440px;
+        margin: 0 auto;
+    }
 
     .logo {
         font-weight: var(--font-weight-bold);
         font-size: 1rem;
+    }
+
+    .back-button {
+        margin-left: auto;
+
     }
 }
 
