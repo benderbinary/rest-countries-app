7e2f9777dd5a2502aa2330406b9215bd022412a6
Eldar Hasanbasic <e.hasanbasic@cheqsite.com>
2024-07-21 14:48:01 +0200

    Fix fail fas

--- src/plugins/fontawesome.ts
+++ src/plugins/fontawesome.ts
@@ -1,6 +1,6 @@
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
-import { faMagnifyingGlass, faChevronLeft, faChevronDown, faChevronUp, faSun, faMoon, faS } from '@fortawesome/free-solid-svg-icons';
+import { faMagnifyingGlass, faChevronLeft, faChevronDown, faChevronUp, faSun, faMoon } from '@fortawesome/free-solid-svg-icons';
 
 library.add(faMagnifyingGlass, faChevronLeft, faChevronDown, faChevronUp, faSun, faMoon);
 
